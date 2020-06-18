f504e0c46d329570983a836b76e48f375d59c7a5
martincorredor <dev.martincorredor@gmail.com>
2020-06-18 01:14:12 +0000

    Print the correction

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -12,6 +12,7 @@ carrie@ubuntu:/debugging$#ifndef HOLBERTON_H
 #include <stdio.h>
 
 void positive_or_negative(int i);
+int largest_number(int a, int b, int c)
 
 #endif /* HOLBERTON_H */
 carrie@ubuntu:/debugging$
