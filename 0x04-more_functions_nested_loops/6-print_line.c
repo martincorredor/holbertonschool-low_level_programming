1e96b9c83690cedf8ec2c87b3231b8b43ed66471
martincorredor <dev.martincorredor@gmail.com>
2020-06-18 23:49:53 +0000

    Draw a stright line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *print_line - Draw a straiht line
+ *@n: integer
+ *
+ *Return - print line n spaces long to stdout; print newline if n=0
+ */
+
+void print_line(int n)
+{
+	int x;
+
+	if (n > 0)
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar('_');
+			_putchar('\n');
+		}
+	}
+	else
+	_putchar('\n');
+}
