e0b98ee29722cc750674d0d2a54aeb1b3177b871
martincorredor <dev.martincorredor@gmail.com>
2020-06-19 00:06:41 +0000

    Print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,22 @@
 #include "holberton.h"
 /**
- *print_line - Draw a straiht line
- *@n: integer
- *
- *Return - print line n spaces long to stdout; print newline if n=0
- */
-
+    * print_line - function that prints a straight line
+      * @n: takes in an integer
+        */
 void print_line(int n)
 {
-	int x;
+		int i;
 
-	if (n > 0)
-	{
-		for (x = 0; x < n; x++)
-		{
-			_putchar('_');
-			_putchar('\n');
-		}
-	}
-	else
-	_putchar('\n');
+			for (i = 0; i < n; i++)
+					{
+								if (n > 0)
+											{
+															_putchar('_');
+																	}
+										else
+													{
+																	_putchar('\n');
+																			}
+											}
+				_putchar('\n');
 }
