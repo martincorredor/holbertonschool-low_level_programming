bc7b7caec2e25011b36208ac2d6bcdd03ef20a88
martincorredor <dev.martincorredor@gmail.com>
2020-06-19 00:51:50 +0000

    Print numbers in lines

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * void more_numbers - prints 10 times the numbers
+ * more_numbers - prints 10 times the numbers
  *
  * Description: Uses a nested loop to print 10 times the numbers
  */
@@ -10,7 +10,7 @@ void more_numbers(void)
 {
 	char i;
 	int counter;
-	
+
 	for (counter = 1; counter <= 10; counter++)
 	{
 		for (i = 0; i <= 14; i++)
--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,11 +3,10 @@
  * print_line - function that prints a straight line
  * @n: takes in an integer
  */
-
 void print_line(int n)
 {
 	int i;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		if (n <= 0)
