db3e3727cae7406deb1a35c20f8cde711ec693f6
martincorredor <dev.martincorredor@gmail.com>
2020-06-19 03:11:20 +0000

    Exclude some numbers

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+  *main - Print numbers from 0 to 100
+  *
+  *Description: Excludes multiples of 3 and 5
+  *
+  *Return: Start
+  */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1 ; n <= 100 ; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else
+			printf("%d", n);
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
