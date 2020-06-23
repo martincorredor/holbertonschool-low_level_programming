cae21bbaac27b2571067a5d06c745c2870e51f17
martincorredor <dev.martincorredor@gmail.com>
2020-06-23 21:30:35 +0000

    Print length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton-h"
+
+/**
+  *_strlen - return length of a string
+  *@s: character
+  *Return: lenght of string
+  */
+
+int _strlen(char *s)
+{
+	int n;
+
+	while (*(s +n))
+	{
+		n++;
+	}
+	return (n);
+}
