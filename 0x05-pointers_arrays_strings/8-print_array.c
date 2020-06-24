f47972617dc55a1b6c6eb008d67fa6f7e3e66f1b
martincorredor <dev.martincorredor@gmail.com>
2020-06-24 04:17:48 +0000

    Prints array elements

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+nclude <stdio.h>
+/**
+ * print_array - prints n elements of an array 
+ * @a: array
+ * @n: number of elements
+ */
+void print_array(int *a, int n)
+{
+	int b;
+
+	for (b = 0; b < n; b++)
+	{
+		if (b != (n - 1))
+			printf("%d, ", a[b]);
+		else
+			printf("%d", a[b]);
+	}
+	printf("\n");
+}
