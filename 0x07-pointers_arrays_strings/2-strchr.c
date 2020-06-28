aea768ce170b31c35cff934c7744bf79022a0adc
martincorredor <dev.martincorredor@gmail.com>
2020-06-28 21:36:50 +0000

    Locates a character in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+  * _strchr - Return a pointer to the first
+  *ocurrence of caracter c in s
+  *@s: string
+  *@c: character to search
+  *Return: string + character of the ocurrence
+  */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	if (s[i] == c)
+		return (s + i);
+	}
+		return ('\0');
+}
