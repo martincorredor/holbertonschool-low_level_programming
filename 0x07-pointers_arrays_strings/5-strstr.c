b42090e234c60ef592dbc1d404abbd4a33321297
martincorredor <dev.martincorredor@gmail.com>
2020-06-29 01:40:51 +0000

    Locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,17 +9,19 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j, k;
+	int i, j;
 
-	for (i = 0; haystck[i] != '\0'; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (k = i, j = 0; needle[j] != '\0'; j++, k++)
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (needle[j] != haystack[k] || haystack[k] == '\0')
+			if (needle[j] != haystack[i] || haystack[i] == '\0')
 				break;
 		}
 		if (needle[j] == '\0')
+		{
 			return (haystack + i);
+		}
 	}
 	return ('\0')
 }
