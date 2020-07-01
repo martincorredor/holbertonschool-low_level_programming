220df1f5e8087a455aa290e9342e37072d56de71
martincorredor <dev.martincorredor@gmail.com>
2020-07-01 20:28:30 +0000

    Print rev string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+  *_print_rev_recursion - Print rev string
+  *@s: string
+  *return: string
+  */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	_print_rev_recursion(s + 1);
+	_putchar (*s);
+}
