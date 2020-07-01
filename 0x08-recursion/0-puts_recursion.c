aad631ad48acd05879a7faa9d2624fe2bb3d730f
martincorredor <dev.martincorredor@gmail.com>
2020-07-01 19:55:03 +0000

    Prints string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+  *_puts_recursion - Print a string
+  *@s: character
+  *return: string
+  */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar ('\n');
+		return;
+	}
+	_putchar (*s);
+	s++;
+	_puts_recursion(s);
+}
