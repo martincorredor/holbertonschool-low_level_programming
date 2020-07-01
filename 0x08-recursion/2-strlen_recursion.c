89a50917376242cf37be3dedcdd5c072ea974f79
martincorredor <dev.martincorredor@gmail.com>
2020-07-01 23:02:22 +0000

    Print length of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+  *_strlen_recursion - length of string
+  *@s: pointer to string
+  *return: number of characters of string
+  */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	s++;
+	return (1 + _strlen_recursion(s));
+}
