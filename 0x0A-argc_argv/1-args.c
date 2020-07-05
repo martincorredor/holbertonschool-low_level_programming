1372004b17e4d00149d92ccbecc5d0e68ef32701
martincorredor <dev.martincorredor@gmail.com>
2020-07-05 22:49:45 +0000

    Prints the number of argumments passed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,7 +8,7 @@
   *Return: 0
   */
 
-int main(int argc, char *argv[] _attribute_((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	if (argc > 0)
 		printf("%d\n", argc - 1);
