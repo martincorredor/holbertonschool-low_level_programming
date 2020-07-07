af9ee29461f0ab536789e2a2ad594112085e4cc9
martincorredor <dev.martincorredor@gmail.com>
2020-07-07 23:52:24 +0000

    Adds pointer to copy

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+  * _strdup - Return a pointer to new copy of string
+  *@str: string to copied
+  *Return: Pointer to copy string
+  */
+
+char *_strdup(char *str)
+{
+	char *copy;
+	unsigned int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	copy = malloc(sizeof(str));
+
+	for (i = 0; str[i] != '\0'; i++)
+		*(copy + i) = *(str + i);
+	copy[i] = '\0';
+
+	return (copy);
+
+}
