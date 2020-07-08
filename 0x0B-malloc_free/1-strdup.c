fe13a7f974fc2368e11e69ce29501fe43b367f8d
martincorredor <dev.martincorredor@gmail.com>
2020-07-08 01:46:12 +0000

    Add pointer to copy

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,20 +11,24 @@
 char *_strdup(char *str)
 {
 	char *copy;
-	unsigned int i;
+	unsigned int i = 0;
+	int len = 0;
+
 
 	if (str == NULL)
 		return (NULL);
 
+	while (*(str + i))
+		len++, i++;
+	len++;
 
-	copy = malloc(sizeof(str));
-
-	for (i = 0; str[i] != '\0'; i++)
-		*(copy + i) = *(str + i);
-	copy[i] = '\0';
+	copy = malloc(sizeof(char) * len);
 
 	if (copy == NULL)
 		return (NULL);
+
+	for (i = 0; i < len; i++)
+		*(copy + i) = *(str + i);
 
 	return (copy);
 
