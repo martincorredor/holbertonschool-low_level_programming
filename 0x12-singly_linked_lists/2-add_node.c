5e164a56b1f0fb1d404b7f07870e7ac6dd2643b1
martincorredor <dev.martincorredor@gmail.com>
2020-07-23 20:49:14 +0000

    Adds a new node at the beginning of list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,34 +1,37 @@
 #include "lists.h"
+/**
+  *_strlen - returns the length of a given string
+  *@s: string to check
+  *Return: Return the length of the string
+  */
+int _strlen(const char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
 
 /**
-  *add_node_end - Add a node in the linked list
+  *add_node - Add a node in the linked list
   *@head: current head of list
   *@str: string
   *Return: pointer to a new node
   */
 
-list_t *add_node_end(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	int i;
 
-	for (i = 0; str[i] != NULL; i++)
-		;
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-	{
-		free(new_node);
 		return (NULL);
-	}
 	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->len = i;
+	new_node->len = _strlen(str);
 	new_node->next = *head;
-	*head = new_node;
+	*head = newnode;
 
-	return (*head);
+	return (new_node);
 }
