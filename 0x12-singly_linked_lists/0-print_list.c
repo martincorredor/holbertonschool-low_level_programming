b9bb41f65f9d5c73850ff14d548578b0f62bb09b
martincorredor <dev.martincorredor@gmail.com>
2020-07-23 21:52:46 +0000

    Prints elements of a list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,12 +10,13 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	for (count = 0; h != NULL; count++)
+	while ( h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
+		count++;
 		h = h->next;
 	}
 	return (count);
