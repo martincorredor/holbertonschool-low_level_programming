cef84b4f759a67d358c763a20dd88e24f576b711
Martin <dev.martincorredor@gmail.com>
2020-08-05 01:03:56 +0000

    Creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+
+/**
+  *_strlen - finds length of string
+  *@str : string o check
+  *Return: length of string
+  */
+int _strlen(char *str)
+{
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+	return (len);
+}
+
+/**
+  *create_file - Creates a file
+  *@filename: name of file
+  *@text_content: file content
+  *Return: 1 on success (create a file) or -1 if fail
+  */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, write_char;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+
+	write_char = write(fd, text_content, _strlen(text_content));
+	if (write_char == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+
+}
