d3ecedccaacdd940a2751adc85fc691e49f14216
Martin <dev.martincorredor@gmail.com>
2020-08-18 05:20:48 +0000

    Pwqrints strings, followed by a new line

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,40 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - main function
+ * @separator: Pointer to the string separator.
+ * @n: The number of arguments.
+ * @...: The strings to print.
+ *
+ * Description: This function prints strings, followed by a new line.
+ *
+ * Return: Nothing.
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list st;
+	int i = 0;
+	char *container;
+
+	va_start(st, n);
+	while (i < ((int) n))
+	{
+		container = va_arg(st, char*);
+		if (!container)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", container);
+		}
+		if (separator && i != ((int) n - 1))
+		{
+			printf("%s", separator);
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(st);
+}
