51880248ed9ba68eb1ddb97c9b7e1e1911acc642
Martin <dev.martincorredor@gmail.com>
2020-08-18 04:04:25 +0000

    Adds header

---
+++ 0x14-bit_manipulation/variadic_functions.h
@@ -0,0 +1,33 @@
+#ifndef VARIADIC_H
+#define VARIADIC_H
+
+/* Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ *struct tokens - struct token.
+ * @pm: The parameters entered
+ * @f: The base function.
+ */
+typedef struct tokens
+{
+	char *pm;
+	void (*f)(va_list p);
+} tokens_t;
+
+
+/* Prototypes */
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void op_c(va_list p);
+void op_i(va_list p);
+void op_f(va_list p);
+void op_s(va_list p);
+
+#endif
