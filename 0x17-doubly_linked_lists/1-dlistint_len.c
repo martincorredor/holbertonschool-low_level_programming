e5a90aef3ec0f840bae5cb8884e51bbbbc98f117
Martin <dev.martincorredor@gmail.com>
2020-09-06 20:00:03 +0000

    returns the number of elements in a linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * dlistint_len - function that return the number of nodes in a list
+ * @h:pointer type struct to list
+ * Return: number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+    /* primero creo un contador tipo: entero >= 0*/
+    /*ya que la funcion es de tipo size_t*/
+    unsigned int number_of_nodes = 0;
+
+    while (h != NULL)
+    {
+        h = h->next;
+        number_of_nodes++;
+    }
+    return (number_of_nodes);
+}
