03176dcdefdb1abe0b72743fa1157eb82c9de509
Martin <dev.martincorredor@gmail.com>
2020-09-06 19:51:14 +0000

    Prints all the elements of a list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,11 @@
-include "lists.h"
+#include "lists.h"
 
 /**
-  *print_dlistint - Return the number of list's nodes
-  *@h: Pointer type struct to list
-  *Return: Number of nodes
-  *
-  */
+ *print_dlistint - Return the number of list's nodes
+ *@h: Pointer type struct to list
+ *Return: Number of nodes
+ *
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
     /*First cretes a count of type unsigned int*/
