ddbca46511e4c2697cc862377e4633b5c6332a4d
martincorredor <dev.martincorredor@gmail.com>
2020-12-14 20:22:15 -0500

    Inserts a new node at a given position.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,51 +1,47 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Inserts a new node at a given position.
- * @h: Pointer to the head.
- * @idx: Index.
- * @n: Integer value.
- *
- * Return: The address of the new node, or NULL if it failed.
- **/
+ *insert_dnodeint_at_index - function that inserts a new node at a
+ * given position.
+ *@h: Linkend list
+ *@idx: is the index of the list where the new node should be added.
+ *@n: List contend
+ *Return: the address of the new node, or NULL if it failed
+ */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *tmp = malloc(sizeof(dlistint_t));
 	unsigned int i = 0;
-	dlistint_t *aux = *h;
-	dlistint_t *newnode = NULL;
 
-	newnode = malloc(sizeof(dlistint_t));
-
-	if (!newnode)
-	{
+	if (h == NULL || *h == NULL || tmp == NULL || new_node == NULL)
 		return (NULL);
-	}
+
+	tmp = *h;
+	new_node->n = n;
 
 	if (idx == 0)
 	{
-		return (add_dnodeint(h, n));
+		new_node->prev = NULL;
+		tmp->prev = new_node;
+		new_node->next = tmp;
+		*h = new_node;
+		return (new_node);
 	}
 
-	while (aux)
+	while (tmp)
 	{
-		if (i == idx - 1 && aux->next != NULL)
+		if (i == (idx - 1))
 		{
-			newnode->n = n;
-			newnode->next = aux->next;
-			newnode->prev = aux;
-			aux->next->prev = newnode;
-			aux->next = newnode;
+			new_node->prev = tmp;
+			new_node->next = tmp->next;
+			tmp->next = new_node;
+			return (new_node);
 		}
-		else if (i == idx - 1 && aux->next == NULL)
-		{
-			return (add_dnodeint_end(h, n));
-		}
-		aux = aux->next;
+		tmp = tmp->next;
 		i++;
 	}
-	if (idx > i)
-	{
-		return (NULL);
-	}
-	return (newnode);
+
+	return (NULL);
 }
