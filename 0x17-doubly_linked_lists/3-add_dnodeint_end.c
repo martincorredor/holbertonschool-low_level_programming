4f4bb52d65a937a6bf1d41a37c81cf527341fffd
martincorredor <dev.martincorredor@gmail.com>
2020-12-14 20:11:35 -0500

    dds a new node at the end of a dlistint_t list.

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,59 +1,39 @@
 #include "lists.h"
 
 /**
- * Create_node - function that create a node
- * @n:node to create
- * Return: the node if was sucess or null if failed
- */
-
-dlistint_t *Create_node(const int n)
-{
-    dlistint_t *nuevo = NULL;
-
-    nuevo = (dlistint_t *)malloc(sizeof(dlistint_t));
-    if (nuevo == NULL)
-        return (NULL);
-
-    if (nuevo != NULL)
-    {
-        nuevo->n = n;
-        nuevo->next = NULL;
-        nuevo->prev = NULL;
-    }
-    return (nuevo);
-}
-
-/**
- * add_dnodeint_end - function that create a node at the end
- * @n:node to create
- * @head:head of a list
- * Return: the node if was sucess or null if failed
- */
-
+ * add_dnodeint_end - Adds a new node at the end of a dlistint_t list.
+ * @head: Pointer to the head.
+ * @n: Element.
+ *
+ * Return: The address of the new element, or NULL if it failed.
+ **/
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *aux = NULL;
-    dlistint_t *nuevo_node = NULL;
+	dlistint_t *newnode = NULL;
+	dlistint_t *list = *head;
 
-    aux = *head;
-    nuevo_node = Create_node(n);
+	newnode = malloc(sizeof(dlistint_t));
 
-    if (*head == NULL)
-    {
-        nuevo_node->prev = NULL;
-        *head = nuevo_node;
-        return (nuevo_node);
-    }
-    /*si se creo el nodo con exito*/
-    if (nuevo_node != NULL)
-    {
-        while (aux->next != NULL)
-        {
-            aux = aux->next; /*aqui nos ubicamos en nuestro ultimo elemnto de la lista*/
-        }
-        nuevo_node->prev = aux;
-        aux->next = nuevo_node;
-        return (nuevo_node);
-    }
-    return (NULL);
+	if (!newnode)
+	{
+		return (NULL);
+	}
+	newnode->n = n;
+	newnode->next = NULL;
+
+	if (list)
+	{
+		while (list->next)
+		{
+			list = list->next;
+		}
+		newnode->prev = list;
+		list->next = newnode;
+	}
+	else
+	{
+		*head = newnode;
+		newnode->next = NULL;
+	}
+	return (newnode);
 }
