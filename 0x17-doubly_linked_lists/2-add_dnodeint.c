7af869b546ac707b3fc93a169645ce0c83dfd740
martincorredor <dev.martincorredor@gmail.com>
2020-12-14 20:11:12 -0500

    Adds a new node at the beginning of a dlistint_t list.

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,54 +1,36 @@
 #include "lists.h"
 
 /**
- * Create_node - function that create a node
- * @n:node to create
- * Return: the node if was sucess or null if failed
- */
-
-dlistint_t *Create_node(const int n)
-{
-    dlistint_t *nuevo = NULL;
-
-    nuevo = malloc(sizeof(dlistint_t));
-
-    if (nuevo == NULL)
-    {
-        free(nuevo);
-        return (NULL);
-    }
-
-    if (nuevo != NULL)
-    {
-        nuevo->n = n;
-        nuevo->next = NULL;
-        nuevo->prev = NULL;
-    }
-    return (nuevo);
-}
-
-/**
- * add_dnodeint - function that return a node if was insert sucess!
- * @n:node to add
- * @head:head of a list
- * Return: the node if was sucess or null if failed
- */
-
-
+ * add_dnodeint - Adds a new node at the beginning of a dlistint_t list.
+ * @head: Pointer to the head.
+ * @n: Element.
+ *
+ * Return: The address of the new element, or NULL if it failed.
+ **/
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *node = NULL;
+	dlistint_t *newnode = NULL;
 
-    node = Create_node(n);
+	newnode = malloc(sizeof(dlistint_t));
 
-    if (node != NULL)
-    {
-        node->prev = NULL;
-        node->next = *head;
-        if (*head != NULL)
-            (*head)->prev = node;
-        *head = node;
-        return (node);
-    }
-    return (NULL);
+	if (!newnode)
+	{
+		return (NULL);
+	}
+
+	newnode->n = n;
+	newnode->prev = NULL;
+
+	if (*head)
+	{
+		newnode->next = *head;
+		(*head)->prev = newnode;
+		*head = newnode;
+	}
+	else
+	{
+		*head = newnode;
+		newnode->next = NULL;
+	}
+	return (newnode);
 }
