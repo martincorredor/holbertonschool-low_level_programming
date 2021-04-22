225460bd71bf4911bae6441324a438f1cfaa50eb
martincorredor <dev.martincorredor@gmail.com>
2021-04-22 14:19:57 -0500

    |UPDATE| Add functions to advanceed tasks

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -20,5 +20,7 @@ int interpolation_search(int *array, size_t size, int value);
 int auxiliar_search(int array[], int start, int end, int key);
  /* Advanced */
 int exponential_search(int *array, size_t size, int value);
-
+int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 #endif
